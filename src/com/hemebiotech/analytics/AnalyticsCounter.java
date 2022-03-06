6f19deb01b7e01e16ae2690c4c1e49291d5fd30b
cariocaphil <cariocaphil@gmail.com>
2022-03-06 19:36:36 +0100

    refactoring via looping through list of symptoms and filling map

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,6 +3,10 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 public class AnalyticsCounter {
 	
@@ -11,33 +15,41 @@ public class AnalyticsCounter {
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
 
-		int i = 0;
-		int headacheCount = 0;
-		int rashCount = 0;
-		int pupilCount = 0;
+		List<String> symptomList = Arrays.asList("headache","rash","pupils");
 
+		HashMap<String, Integer> symptomMap = new HashMap<String, Integer>();
+		for (int i =0; i < symptomList.size(); i++) {
+			symptomMap.put(symptomList.get(i), 0);
+		}
+		int j = 0;
 		while (line != null) {
-			i++;
+			j++;
 			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headacheCount++;
-				System.out.println("number of headaches: " + headacheCount);
-			}
-			else if (line.equals("rash")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
+
+			for(int k =0; k < symptomList.size(); k++) {
+				if (line.contains(symptomList.get(k))) {
+					symptomMap.put(symptomList.get(k), symptomMap.get(symptomList.get(k)) + 1);
+				}
 			}
 
 			line = reader.readLine();	// get another symptom
 		}
+
+		System.out.printf(String.valueOf(symptomMap));
+
 		reader.close();
+
 		// next generate output
 		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("pupils: " + pupilCount + "\n");
+
+		symptomMap.forEach((key, value) -> {
+			try {
+				writer.write(key + ": " + value.toString() + "\n");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+
 		writer.close();
 	}
 }
