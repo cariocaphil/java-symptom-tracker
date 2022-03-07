ca745dbf0c1070b678402321bb6e161363935db2
cariocaphil <cariocaphil@gmail.com>
2022-03-07 17:43:02 +0100

    refactoring via filling map now regarding whatever symptoms mentioned in input file

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -15,21 +15,17 @@ public class AnalyticsCounter {
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
 
-		List<String> symptomList = Arrays.asList("headache","rash","pupils");
-
 		HashMap<String, Integer> symptomMap = new HashMap<String, Integer>();
-		for (int i =0; i < symptomList.size(); i++) {
-			symptomMap.put(symptomList.get(i), 0);
-		}
+
 		int j = 0;
 		while (line != null) {
 			j++;
 			System.out.println("symptom from file: " + line);
 
-			for(int k =0; k < symptomList.size(); k++) {
-				if (line.contains(symptomList.get(k))) {
-					symptomMap.put(symptomList.get(k), symptomMap.get(symptomList.get(k)) + 1);
-				}
+			if (!symptomMap.containsKey(line)) {
+				symptomMap.put(line, 1);
+			} else {
+				symptomMap.put(line, symptomMap.get(line) + 1);
 			}
 
 			line = reader.readLine();	// get another symptom
