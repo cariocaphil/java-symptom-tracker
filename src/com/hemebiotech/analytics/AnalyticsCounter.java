0c82b425bd62fedd19c23f85326f76d73c36667d
cariocaphil <cariocaphil@gmail.com>
2022-03-07 18:12:57 +0100

    refactoring using ReadSymptomDataFromFile

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,10 +1,7 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -12,28 +9,20 @@ public class AnalyticsCounter {
 	
 	public static void main(String args[]) throws Exception {
 		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+		ReadSymptomDataFromFile reader = new ReadSymptomDataFromFile("symptoms.txt");
+		List<String> symptomsInFileList = reader.GetSymptoms();
 
 		HashMap<String, Integer> symptomMap = new HashMap<String, Integer>();
 
-		int j = 0;
-		while (line != null) {
-			j++;
-			System.out.println("symptom from file: " + line);
-
-			if (!symptomMap.containsKey(line)) {
-				symptomMap.put(line, 1);
+		for (String symptomString : symptomsInFileList) {
+			if (!symptomMap.containsKey(symptomString)) {
+				symptomMap.put(symptomString, 1);
 			} else {
-				symptomMap.put(line, symptomMap.get(line) + 1);
+				symptomMap.put(symptomString, symptomMap.get(symptomString) + 1);
 			}
-
-			line = reader.readLine();	// get another symptom
 		}
 
 		System.out.printf(String.valueOf(symptomMap));
-
-		reader.close();
 
 		// next generate output
 		FileWriter writer = new FileWriter ("result.out");
